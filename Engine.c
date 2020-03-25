beea77ca766befc7989d6c9abc3d0862495b8638
ArchLuke <lkong@bu.edu>
2020-03-25 15:39:17 -0400

    file hierarchy

---
+++ Engine.c
@@ -0,0 +1,18 @@
+#include <bishop.c>
+#include <config.h>
+#include <defs.h>
+#include <eval.c>
+#include <king.c>
+#include <knight.c>
+#include <LichessApi.c>
+#include <pawn.c>
+#include <queen.c>
+#include <rook.c>
+
+int main()
+{
+
+
+//uci loop
+	return 0;
+}
