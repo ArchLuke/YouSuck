6bebfa937be01a20a1a942c04767c734d55a36fb
ArchLuke <lkong@bu.edu>
2020-03-25 21:53:09 -0400

    main

--- Engine.c
+++ Engine.c
@@ -1108,7 +1108,42 @@ void UpdateListsMaterial(S_BOARD *pos) {
 }
 int main()
 {
+AllInit();	
+	
+	S_BOARD pos[1];
+    	S_SEARCHINFO info[1];   
+	pos->PvTable->pTable = NULL;
+    	InitPvTable(pos->PvTable);
+	setbuf(stdin, NULL);
+ 	setbuf(stdout, NULL);
+	
+	char line[256];
+	while (TRUE) {
+		memset(&line[0], 0, sizeof(line));
 
+		fflush(stdout);
+		if (!fgets(line, 256, stdin))
+			continue;
+		if (line[0] == '\n')
+			continue;
+		if (!strncmp(line, "uci",3)) {			
+			Uci_Loop(pos, info);
+			if(info->quit == TRUE) break;
+			continue;
+		} else if (!strncmp(line, "xboard",6))	{
+			XBoard_Loop(pos, info);
+			if(info->quit == TRUE) break;
+			continue;
+		} else if (!strncmp(line, "vice",4))	{
+			Console_Loop(pos, info);
+			if(info->quit == TRUE) break;
+			continue;
+		} else if(!strncmp(line, "quit",4))	{
+			break;
+		}
+	}
+	
+	free(pos->PvTable->pTable);
 
 //uci loop
 	return 0;
